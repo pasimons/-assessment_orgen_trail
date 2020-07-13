f40258e97bd70b2763e4133b827e7fbb68f38954
patricia simons <trishasimons16@gmail.com>
2020-07-13 11:39:47 -0400

    save

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
-class traveler{
-    constructor (name){
+class traveler {
+    constructor(name) {
         this.name = name
     }
-    hunt()  {
+    hunt() {
         this.food += 2
     }
-        eat() {
-           if(this.isHealthy  = 1){
-               return false
-           }else {
-               return true
-           }
-        }   
+    eat() {
+        if (this.isHealthy = 1) {
+            return false
+        } else {
+            return true
+        }
+    }
 }
 
 
-Let myWagon = new Wagon(5)
-Constructor (capacity) {}
+class NewWagon {
+    constructor(capacity) {
+        this.space = capacity
+    }
+}
 
-let passengers = [] 
-getAvailableSeatCount() 
-join(traveler)
-shouldQuarantine()
-totalFood()
+
+
+let passengers = []
+getAvailableSeatCount() {
+    for (let index = 0; index < 5; index--) {
+return getAvailableSeatCount
+    }
+    
+
+
+    join(traveler){
+        if (this.space === 0){
+            return false
+        }else{
+return true
+        }
+    }
+
+    shouldQuarantine(){
+        if(this.helath ===0 ){
+            return helathy
+        }else{
+            return sick
+        }
+    }
+    totalFood(){
+
+    }
